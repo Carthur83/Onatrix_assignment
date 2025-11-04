349c1abc60a2961e91a456ea94ae63f0e8c3419c
Carthur83 <christian_A_83@hotmail.com>
2025-11-04 11:26:21 +0100

    Update Question Form

--- Onatrix_assignment/Services/FormSubmissionService.cs
+++ Onatrix_assignment/Services/FormSubmissionService.cs
@@ -34,4 +34,30 @@ public class FormSubmissionService(IContentService contentService)
             return false;
 		}
     }
+
+    public bool SaveQuestionRequest(QuestionFormViewModel model)
+    {
+        try
+        {
+            var container = _contentService.GetRootContent().FirstOrDefault(x => x.ContentType.Alias == "formSubmissions");
+            if (container == null)
+                return false;
+
+            var requestName = $"{DateTime.Now:yyyy-MM-dd HH:mm} - {model.Name}";
+            var request = _contentService.Create(requestName, container, "questionRequest");
+
+            request.SetValue("questionFormName", model.Name);
+            request.SetValue("questionFormEmail", model.Email);
+            request.SetValue("questionFormQuestion", model.Question);
+
+            var saveResult = _contentService.Save(request);
+
+            return saveResult.Success;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
 }
